b665b2c0720640ca6a85aba6a24358a07d4d7b81
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-07-28 22:09:19 -0300

    added PGPORT env variable

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PGPORT
 } = process.env;
 
 let sequelize =
@@ -12,7 +12,7 @@ let sequelize =
       database: DB_NAME,
       dialect: 'postgres',
       host: DB_HOST,
-      port: 5432,
+      port: PGPORT,
       username: DB_USER,
       password: DB_PASSWORD,
       pool: {
